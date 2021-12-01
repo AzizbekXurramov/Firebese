0c7ea8d3b6bd8c4eb119dc5d521d3188390d1a11
Azizbek <azizbekxurramov0102@gmail.com>
2021-12-01 15:53:00 +0500

    Ikkinchi Ferebase

--- app/src/main/java/uz/azizbekxurramov/firebese/MainActivity.kt
+++ app/src/main/java/uz/azizbekxurramov/firebese/MainActivity.kt
@@ -1,11 +1,15 @@
 package uz.azizbekxurramov.firebese
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /** Bu yerda firebase ni githab gab ga joylash uchun uzgratirish kirityapman*/
+        Log.d("AZIZBEK", "Firebase mavzusi")
     }
 }
